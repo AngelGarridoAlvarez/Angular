abe32c05e69a7ed92551057c500b3369147a23ed
Angel Garrido Alvarez <angelgarridoalvarez@gmail.com>
2020-10-16 12:29:31 +0200

    finalizado hasta 14.1 Crear Formularios

--- learningAngular/src/app/contacto/contacto.component.ts
+++ learningAngular/src/app/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ContactoUsuario} from "../models/usuario";
 
 @Component({
   selector: 'app-contacto',
@@ -6,10 +7,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contacto.component.scss']
 })
 export class ContactoComponent implements OnInit {
+  public usuario: ContactoUsuario; //* Añadimos la propiedad usuario y decimos que es un objeto de tipo Usuario en nuestro contacto.component.ts
 
-  constructor() { }
+  constructor() {
+    this.usuario = new ContactoUsuario('','','','')//Dejo los campos vacíos para rellenarlos con el formular
+  }
 
   ngOnInit(): void {
+  }
+
+  enviar(){
+    console.log('evento enviar lanzado')//Cuando pulse a enviar se muestra este mensaje
+    console.log(this.usuario)//Me recoge el objeto usuario que se ha lanzado
   }
 
 }
